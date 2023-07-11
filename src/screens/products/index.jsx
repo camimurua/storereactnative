848ffb1429fec2274814617c0d5916d79ebc044c
camimurua <camilamurua99@gmail.com>
2023-07-11 19:31:10 -0300

    Sumo buscador de productos

--- src/screens/products/index.jsx
+++ src/screens/products/index.jsx
@@ -1,11 +1,44 @@
-import { View, Text, Button } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { styles } from "./styles";
+import { Input } from '../../components';
+import { useState } from 'react';
+import { COLORS } from '../../themes';
+import { Ionicons } from '@expo/vector-icons'; 
 
 const Products = ({onHandleGoBack}) => {
+    const [search, setSearch] = useState('');
+    const [borderColor, setBorderColor] = useState(COLORS.primary);
+    const onHandleBlur = () => {
+        
+    }
+    const onHandleChangeText = (text) => {
+        setSearch(text);
+    }
+    const onHandleFocus = () => {
+
+    }
     return (
         <View style={styles.container}> 
-            <Button title="Go back" onPress={onHandleGoBack}/>
-            <Text>Category selected</Text>
+            <TouchableOpacity style={styles.goBack} onPress={onHandleGoBack}>
+                <Ionicons name="arrow-back-circle" size={30} color={COLORS.black} />
+                <Text style={styles.goBackText}>Go Back</Text>
+            </TouchableOpacity>
+            <View style={styles.header}>
+                <Input 
+                    onHandleBlur={onHandleBlur}
+                    onHandleChangeText={onHandleChangeText}
+                    onHandleFocus={onHandleFocus}
+                    value={search}
+                    placeholder='Search'
+                    borderColor={borderColor}
+                />
+                <Ionicons name="search-circle" size={40} color={COLORS.black} />
+                {
+                    search.length > 0 &&
+                    <Ionicons name="close-circle" size={40} color={COLORS.black} />
+                }
+                
+            </View>
         </View>
     );
 }
